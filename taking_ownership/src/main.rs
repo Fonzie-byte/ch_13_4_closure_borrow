a7e58f439bf457e75a10f5c4c1fd887364005d2c
Fons <40026023+Fonzie-byte@users.noreply.github.com>
2025-08-30 01:27:55 +0200

    feat: Better, multithreaded taking-ownership example.

--- taking_ownership/src/main.rs
+++ taking_ownership/src/main.rs
@@ -1,8 +1,18 @@
+use std::thread;
+
 fn main() {
     let nums = vec![1, 2, 3];
     println!("Before closure declaration:\t{nums:?}");
 
+    /*
+    // Most basic example that moves a value.
     let takes_ownership = move || println!("From within the closure:\t{nums:?}");
 
     takes_ownership();
+    */
+
+    // Moving is especially common when moving to another thread.
+    thread::spawn(move || println!("From within closure:\t{nums:?}"))
+        .join() // Waits for the thread to finish and join back with the main process.
+        .unwrap();
 }
